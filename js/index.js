717536ccb79158edc5acb08676b0fc9dc5eccb1e
chandresh <patidarchandresh2002@gmail.com>
2022-08-30 18:43:24 +0530

    image dialog working

--- js/index.js
+++ js/index.js
@@ -52,3 +52,75 @@ function makingNavWork(){
   })
 }
 makingNavWork();
+
+function toogleDialog(){
+  const dialogBox = document.getElementById('dialog-box');
+  dialogBox.classList.toggle('active');
+}
+function makeToggleformwork(){
+const dialogclose = document.getElementsByClassName('close-dialog-button')[0];
+const dialogBox = document.getElementById('dialog-box');
+dialogclose.addEventListener('click',()=>{
+  dialogBox.classList.toggle('active');
+})
+const buttons = Array.from(document.getElementsByClassName('open-enquire-form'));
+buttons.forEach((ele)=>{
+  ele.addEventListener('click',()=>{
+    toogleDialog();
+  })
+})
+}
+makeToggleformwork();
+
+function imageZoom(){
+  const images = Array.from(document.getElementsByClassName('image-zoom'));
+      const imageDialogCon= document.querySelector('.image-dialog');
+function showImageDialog(src){
+    const imageDialog = document.querySelector('.image-dialog img');
+    imageDialogCon.classList.toggle('active');
+    imageDialog.setAttribute('src',src);
+  }
+  const closeImageDialogButton = document.getElementsByClassName('close-image-dialog-button')[0];
+  closeImageDialogButton.addEventListener('click',()=>{
+    imageDialogCon.classList.toggle('active');
+  })
+  images.forEach(ele=>{
+    ele.addEventListener('click',()=>{
+      showImageDialog(ele.getAttribute('src'));
+    })
+  })
+  
+}
+imageZoom();
+
+function imageDialog(){
+  const imagesSrcs = Array.from( document.getElementsByClassName('image-zoom')).map(ele=>{
+    return ele.getAttribute('src');
+  })
+  const nextButton = document.querySelector('.next-image-dialog');
+  const prevButton = document.querySelector('.prev-image-dialog');
+
+  const imageDialog = document.querySelector('.image-dialog img');
+
+  function currentImgSrcIndex(){
+    const index = imagesSrcs.indexOf(imageDialog.getAttribute('src'));
+    console.log('current index',index);
+      return index;
+  }
+  function switchImgTo(index){
+    if(imagesSrcs[index]){
+      console.log('called');
+      imageDialog.setAttribute('src',imagesSrcs[index]);
+
+    }
+  }
+  nextButton.addEventListener('click',()=>{
+    let currentIndex = currentImgSrcIndex();
+    switchImgTo(++currentIndex)
+    console.log('new',currentIndex)
+  })
+  prevButton.addEventListener('click',()=>{
+    let currentIndex = currentImgSrcIndex();
+    switchImgTo(--currentIndex)})
+}
+imageDialog();
